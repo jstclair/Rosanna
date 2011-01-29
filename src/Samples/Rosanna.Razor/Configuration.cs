912cf3e5525308ccad74712f8d3743ddff220a49
thedersen <thedersen@gmail.com>
2011-01-29 22:51:39 +0100

    Added Razor sample (not done)

---
+++ src/Samples/Rosanna.Razor/Configuration.cs
@@ -0,0 +1,10 @@
+namespace Rosanna.Razor
+{
+    public class Configuration : RosannaConfiguration
+    {
+        public Configuration()
+        {
+            Author = "Thomas";
+        }
+    }
+}
